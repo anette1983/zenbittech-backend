6c377b2d52d492b9e522217751c8ed7469849b1c
Hanna <anna_kon@bigmir.net>
2023-10-16 01:18:36 +0300

    deleted comments

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,31 +1,22 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-// const gravatar = require("gravatar");
-// const Jimp = require("jimp");
-// const path = require("path");
-// const fs = require("fs/promises");
 const { nanoid } = require("nanoid");
 
 const { User } = require("../../models/user");
 const { HttpError, ctrlWrapper, sendEmail } = require("../../helpers");
 const { SECRET_KEY, BASE_URL } = process.env;
 
-// const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
-
 const register = async (req, res) => {
   const { email, password } = req.body;
-  // const user = await User.findOne({ email });
   const user = await User.findOne({ where: { email } });
   if (user) {
     throw HttpError(409, "Email already in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  // const avatarURL = gravatar.url(email);
   const verificationToken = nanoid();
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
-    // avatarURL,
     verificationToken,
   });
   const verifyEmail = {
@@ -38,7 +29,6 @@ const register = async (req, res) => {
     res.status(201).json({
       user: {
         email: newUser.email,
-        // subscription: newUser.subscription
       },
     });
   } catch (error) {
@@ -48,7 +38,6 @@ const register = async (req, res) => {
 
 const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
-  // const user = await User.findOne({ verificationToken });
   const user = await User.findOne({ where: { verificationToken } });
   if (!user) {
     throw HttpError(404, "User not found");
@@ -56,14 +45,11 @@ const verifyEmail = async (req, res) => {
   if (user.verify) {
     throw HttpError(404, "Email is already verified");
   }
-  // await User.findByIdAndUpdate(user._id, {
-  //   verify: true,
-  //   verificationToken: null,
-  // });
+
   await User.update(
     { verify: true, verificationToken: null },
     {
-      where: { id: user.id }, // Assuming your primary key is named 'id'
+      where: { id: user.id },
     }
   );
   res.json({
@@ -73,7 +59,6 @@ const verifyEmail = async (req, res) => {
 
 const resendVerifyEmail = async (req, res) => {
   const { email } = req.body;
-  // const user = await User.findOne({ email });
   const user = await User.findOne({ where: { email } });
   if (!user) {
     throw HttpError(404, "Email not found");
@@ -111,33 +96,26 @@ const login = async (req, res) => {
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-  // await User.findByIdAndUpdate(user._id, { token });
   user.token = token;
   await user.save();
-  // await User.update({ token }, { where: { id } });
   res.json({
     token,
     user: {
       email: user.email,
-      // subscription: user.subscription
     },
   });
 };
 
 const getCurrent = async (req, res) => {
-  // const { email, subscription } = req.user;
   const { email } = req.user;
   res.json({
     email,
-    // subscription,
   });
 };
 
 const logout = async (req, res) => {
   const { id } = req.user;
   // цей параметр є у об'єкті юзера, який робить запит, нічого додатково вводити не треба
-  // await User.findByIdAndUpdate(_id, { token: "" });
-  // await User.update({ token: "" }, { where: { id } });
   const user = await User.findOne({ where: { id } });
   if (user) {
     user.token = null;
@@ -149,49 +127,6 @@ const logout = async (req, res) => {
   });
 };
 
-// const updateSubscription = async (req, res) => {
-//   const { _id } = req.user;
-//   const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
-//   if (!result) {
-//     throw HttpError(404, `User with id = ${_id} not found`);
-//   }
-//   res.status(200).json({
-//     status: "success",
-//     code: 200,
-//     message: "User subscription updated successfully",
-//     data: {
-//       id: result._id,
-//       email: result.email,
-//       subscription: result.subscription,
-//     },
-//   });
-// };
-
-// const updateAvatar = async (req, res) => {
-//   const { _id } = req.user;
-//   const { path: tempUpload, originalname } = req.file;
-//   const filename = `${_id}_${originalname}`;
-//   const resultUpload = path.join(avatarsDir, filename);
-//   // await fs.rename(tempUpload, resultUpload);
-//   await Jimp.read(tempUpload)
-//     .then((image) => {
-//       return image.cover(250, 250).write(resultUpload);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       throw new Error("Failed to resize the avatar image.");
-//     });
-
-//   // Delete the temporary upload file
-//   await fs.unlink(tempUpload);
-
-//   const avatarURL = path.join("avatars", filename);
-//   await User.findByIdAndUpdate(_id, { avatarURL });
-//   res.json({
-//     avatarURL,
-//   });
-// };
-
 module.exports = {
   register: ctrlWrapper(register),
   verifyEmail: ctrlWrapper(verifyEmail),
@@ -199,10 +134,7 @@ module.exports = {
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
-  // updateSubscription: ctrlWrapper(updateSubscription),
-  // updateAvatar: ctrlWrapper(updateAvatar),
 };
-
 
 // const createDeal = async (deal) => {
 //   await Deal.create({
